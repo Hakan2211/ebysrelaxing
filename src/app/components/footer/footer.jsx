28e1c1af49f3f1061484900a0c6eaf0bf5b4ec18
hakan <hbilgic1992@gmail.com>
2024-02-04 18:05:29 +0100

    delete red border in footer

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import Instagram from './instagram';
 
 function Footer() {
   return (
-    <div className="bg-gray-800 border border-red-800 h-[13rem] md:h-[8rem]">
+    <div className="bg-gray-800  h-[13rem] md:h-[8rem]">
       <div className="flex flex-col md:flex-row  md:justify-between items-center py-8 px-8">
         <div className="flex items-center gap-4 mb-6 md:mb-0">
           <Logo className="w-12 h-12 ml-4" />
