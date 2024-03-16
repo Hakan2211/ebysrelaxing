8b2e4771feb326b5ca12b57ccf1d6304c603d2ef
hakan <hbilgic1992@gmail.com>
2024-03-16 11:20:45 +0100

    add favicon and calendly embed text pop to price section

---
+++ src/app/components/calendly/PopupWidget.js
@@ -0,0 +1,56 @@
+'use client';
+
+import React, { useEffect } from 'react';
+
+const CalendlyEmbed = ({ url }) => {
+  useEffect(() => {
+    // Dynamically load the Calendly script
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Dynamically load the Calendly CSS
+    const link = document.createElement('link');
+    link.href = 'https://assets.calendly.com/assets/external/widget.css';
+    // link.backgroundColor = '#e1d5bf';
+    // link.textColor = '#6a604d';
+    // link.primaryColor = '#fff7f2';
+    // link.type = 'text/css';
+    link.rel = 'stylesheet';
+
+    document.head.appendChild(link);
+
+    return () => {
+      // Cleanup the script and style
+      document.body.removeChild(script);
+      document.head.removeChild(link);
+    };
+  }, []);
+
+  const handleCalendlyPopup = () => {
+    // Ensure Calendly is loaded
+    if (window.Calendly) {
+      window.Calendly.initPopupWidget({
+        url: url,
+      });
+    }
+  };
+
+  return (
+    <div
+      className="text-black group-hover:text-white text-xl duration-300 transition-colors"
+      onClick={handleCalendlyPopup}
+      style={{ cursor: 'pointer' }}
+    >
+      Termin buchen
+    </div>
+  );
+};
+
+export default CalendlyEmbed;
+
+// <!-- Calendly Inline-Widget Beginn -->
+// <div class="calendly-inline-widget" data-url="https://calendly.com/ebysrelaxing/business-yoga?background_color=e1d5bf&text_color=6a604d&primary_color=fff7f2" style="min-width:320px;height:700px;"></div>
+// <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+// <!-- Calendly Inline-Widget Ende -->
--- src/app/components/landing-section/price-section.jsx
+++ src/app/components/landing-section/price-section.jsx
@@ -7,10 +7,53 @@ import {
   CardContent,
   CardFooter,
 } from '@/components/ui/card';
+import CalendlyEmbed from '../calendly/PopupWidget';
 
 export default function PriceSection() {
   return (
     <div className="grid gap-6  md:gap-8  grid-cols-1 font-medium  md:grid-cols-3">
+      <Card className="flex flex-col text-slate-800 border-none bg-[var(--bg-background-color-5)]">
+        <CardHeader>
+          <div className="flex items-baseline justify-between">
+            <CardTitle className="text-4xl font-medium tracking-wide">
+              Gruppenevent
+            </CardTitle>
+            {/* <div className="text-4xl font-bold">499$</div> */}
+            <div className="text-4xl tracking-wide">49€</div>
+          </div>
+          <CardDescription className=" text-slate-800 text-xl tracking-wide">
+            Onlineworkshop
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="text-xl tracking-wide">
+          <ul className="space-y-2">
+            <li className="flex items-center">
+              <CheckIcon className="mr-2 inline-block h-4 w-4" />
+              Yoga
+            </li>
+            <li className="flex items-center">
+              <CheckIcon className="mr-2 inline-block h-4 w-4" />
+              Meditation
+            </li>
+            <li className="flex items-center">
+              <CheckIcon className="mr-2 inline-block h-4 w-4" />
+              Klangbad
+            </li>
+            <li className="flex items-center">
+              <CheckIcon className="mr-2 inline-block h-4 w-4" />
+              EFT (Emotional Freedom Technique)
+            </li>
+          </ul>
+        </CardContent>
+        <CardFooter className="group w-fit">
+          <Button
+            className="bg-[var(--bg-background-color-6)] group-hover:text-white duration-300 transition-colors"
+            variant="outline"
+          >
+            <CalendlyEmbed url="https://calendly.com/ebysrelaxing/gruppenevent?background_color=fff7f2&text_color=6a604d&primary_color=6a604d" />
+          </Button>
+        </CardFooter>
+      </Card>
       <Card className="flex flex-col border-none  text-slate-800 bg-[var(--bg-background-color-5)]">
         <CardHeader>
           <div className="flex items-baseline justify-between">
@@ -44,56 +87,16 @@ export default function PriceSection() {
             </li>
           </ul>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="group w-fit">
           <Button
-            className="bg-[var(--bg-background-color-6)]"
+            className="bg-[var(--bg-background-color-6)] group-hover:text-white duration-300 transition-colors"
             variant="outline"
           >
-            <span className="text-xl">Termin buchen</span>
+            <CalendlyEmbed url="https://calendly.com/ebysrelaxing/business-yoga?background_color=fff7f2&text_color=6a604d&primary_color=6a604d" />
           </Button>
         </CardFooter>
       </Card>
-      <Card className="flex flex-col text-slate-800 border-none bg-[var(--bg-background-color-5)]">
-        <CardHeader>
-          <div className="flex items-baseline justify-between">
-            <CardTitle className="text-4xl font-medium tracking-wide">
-              Individuelle Betreuung
-            </CardTitle>
-            {/* <div className="text-4xl font-bold">499$</div> */}
-          </div>
-          <CardDescription className=" text-slate-800 text-xl tracking-wide">
-            Preis auf Anfrage
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="text-xl tracking-wide">
-          <ul className="space-y-2">
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 inline-block h-4 w-4" />
-              Yoga
-            </li>
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 inline-block h-4 w-4" />
-              Meditation
-            </li>
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 inline-block h-4 w-4" />
-              Klangbad
-            </li>
-            <li className="flex items-center">
-              <CheckIcon className="mr-2 inline-block h-4 w-4" />
-              EFT (Emotional Freedom Technique)
-            </li>
-          </ul>
-        </CardContent>
-        <CardFooter>
-          <Button
-            className="bg-[var(--bg-background-color-6)]"
-            variant="outline"
-          >
-            <span className="text-xl">Termin buchen</span>
-          </Button>
-        </CardFooter>
-      </Card>
+
       <Card className="flex flex-col text-slate-800 border-none bg-[var(--bg-background-color-5)]">
         <CardHeader>
           <div className="flex md:flex-col lg:flex-row items-baseline justify-between">
@@ -127,12 +130,12 @@ export default function PriceSection() {
             </li>
           </ul>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="group w-fit">
           <Button
-            className="bg-[var(--bg-background-color-6)]"
+            className="bg-[var(--bg-background-color-6)] group-hover:text-white duration-300 transition-colors"
             variant="outline"
           >
-            <span className="text-xl">Termin buchen</span>
+            <CalendlyEmbed url="https://calendly.com/ebysrelaxing/astrologie?background_color=fff7f2&text_color=6a604d&primary_color=6a604d" />
           </Button>
         </CardFooter>
       </Card>
