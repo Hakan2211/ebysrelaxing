25e7e0db30a81fb7aa98b958db3048fcdf2a9ce8
hakan <hbilgic1992@gmail.com>
2024-03-18 21:40:07 +0100

    create leistungen route page

--- src/app/components/landing-section/personal-section.jsx
+++ src/app/components/landing-section/personal-section.jsx
@@ -2,6 +2,7 @@ import { didact, aboreto, maitree } from '@/app/fonts/fonts';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Instagram from '../footer/instagram';
+import Link from 'next/link';
 
 function PersonalSection() {
   return (
@@ -17,21 +18,30 @@ function PersonalSection() {
           <h2>Ich bin Deine Entspannungstrainerin und Yoga-Coach</h2>
           <p>Ich helfe Dir dabei, dein Selbstbewusstsein zu finden,</p>
           <p>um Dich in deinem wundervollen Körper wohl zu fühlen.</p>
-          <Button className="mt-8 p-6 bg-[var(--bg-background-color-6)] duration-500 ease-in-out  transition-colors group hover:bg-[var(--bg-background-color-7)]">
-            <a
-              href="https://www.instagram.com/ebysrelaxing"
-              target="_blank"
-              className="flex gap-2 group-hover:text-slate-200 "
-            >
-              <span className="text-xl font-bold  tracking-wide text-slate-800 duration-500 ease-in-out transition-colors group-hover:text-slate-200">
-                Instagram
-              </span>
-              <Instagram className="w-6 translate-y-[2px]  fill-slate-800 duration-500 ease-in-out transition-colors group-hover:fill-slate-200 " />
-            </a>
-          </Button>
+          <div className="flex flex-col gap-4 md:flex-row ">
+            <Button className="mt-8 p-6 bg-[var(--bg-background-color-6)] duration-500 ease-in-out  transition-colors group hover:bg-[var(--bg-background-color-7)]">
+              <Link href="/about">
+                <span className="text-xl font-bold  tracking-wide text-slate-800 duration-500 ease-in-out transition-colors group-hover:text-slate-200">
+                  Mehr über mich
+                </span>
+              </Link>
+            </Button>
+            <Button className="mt-8 p-6 bg-[var(--bg-background-color-6)] duration-500 ease-in-out  transition-colors group hover:bg-[var(--bg-background-color-7)]">
+              <a
+                href="https://www.instagram.com/ebysrelaxing"
+                target="_blank"
+                className="flex gap-2 group-hover:text-slate-200 "
+              >
+                <span className="text-xl font-bold  tracking-wide text-slate-800 duration-500 ease-in-out transition-colors group-hover:text-slate-200">
+                  Instagram
+                </span>
+                <Instagram className="w-6 translate-y-[2px]  fill-slate-800 duration-500 ease-in-out transition-colors group-hover:fill-slate-200 " />
+              </a>
+            </Button>
+          </div>
         </div>
       </div>
-      <div className="md:w-96 ">
+      <div className="md:w-96 p-4 md:p-0 ">
         <div className="rounded-full">
           <Image
             className="rounded-full"
--- src/app/leistungen/page.jsx
+++ src/app/leistungen/page.jsx
@@ -1,7 +1,88 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
+import CalendlyEmbed from '../components/calendly/PopupWidget';
 
 function Leistungen() {
-  return <div>Leistungen</div>;
+  return (
+    <div className="md:col-start-2 md:col-end-5 col-start-1 col-end-6 text-slate-800 p-4">
+      <h2 className="text-2xl tracking-wide leading-relaxed underline underline-offset-4">
+        Onlineworkshops
+      </h2>
+      <p className="mt-5 text-xl tracking-wider leading-relaxed">
+        Ich bin Meditations- und Yoga-Lehrerin und ich liebe es, Menschen zu
+        helfen, sich zu entspannen und zu erholen. Ich bin spezialisiert auf
+        Yoga und Meditation für Anfänger und Fortgeschrittene. Das Ziel meiner
+        Arbeit ist es, Menschen zu helfen, sich zu entspannen und zu erholen.
+      </p>
+
+      <p className="text-xl tracking-wider leading-relaxed">
+        Ich bin Meditations- und Yoga-Lehrerin und ich liebe es, Menschen zu
+        helfen, sich zu entspannen und zu erholen. Ich bin spezialisiert auf
+        Yoga und Meditation für Anfänger und Fortgeschrittene. Das Ziel meiner
+        Arbeit ist es, Menschen zu helfen, sich zu entspannen und zu erholen.
+      </p>
+      <div className="group w-fit">
+        <Button
+          className="mb-10 mt-5 bg-[var(--bg-background-color-6)] group-hover:text-white duration-300 transition-colors"
+          variant="outline"
+        >
+          <CalendlyEmbed url="https://calendly.com/ebysrelaxing/gruppenevent?background_color=fff7f2&text_color=6a604d&primary_color=6a604d" />
+        </Button>
+      </div>
+
+      <h2 className="text-2xl tracking-wide leading-relaxed underline underline-offset-4">
+        Business Yoga
+      </h2>
+
+      <p className=" text-xl tracking-wider leading-relaxed mt-5">
+        Ich bin Meditations- und Yoga-Lehrerin und ich liebe es, Menschen zu
+        helfen, sich zu entspannen und zu erholen. Ich bin spezialisiert auf
+        Yoga und Meditation für Anfänger und Fortgeschrittene. Das Ziel meiner
+        Arbeit ist es, Menschen zu helfen, sich zu entspannen und zu erholen.
+      </p>
+
+      <p className=" text-xl tracking-wider leading-relaxed">
+        Ich bin Meditations- und Yoga-Lehrerin und ich liebe es, Menschen zu
+        helfen, sich zu entspannen und zu erholen. Ich bin spezialisiert auf
+        Yoga und Meditation für Anfänger und Fortgeschrittene. Das Ziel meiner
+        Arbeit ist es, Menschen zu helfen, sich zu entspannen und zu erholen.
+      </p>
+      <div className="group w-fit">
+        <Button
+          className=" mb-10 mt-5 bg-[var(--bg-background-color-6)] group-hover:text-white duration-300 transition-colors"
+          variant="outline"
+        >
+          <CalendlyEmbed url="https://calendly.com/ebysrelaxing/business-yoga?background_color=fff7f2&text_color=6a604d&primary_color=6a604d" />
+        </Button>
+      </div>
+
+      <h2 className="text-2xl tracking-wide leading-relaxed underline underline-offset-4">
+        Astrologie
+      </h2>
+
+      <p className=" text-xl tracking-wider leading-relaxed mt-5">
+        Ich bin Meditations- und Yoga-Lehrerin und ich liebe es, Menschen zu
+        helfen, sich zu entspannen und zu erholen. Ich bin spezialisiert auf
+        Yoga und Meditation für Anfänger und Fortgeschrittene. Das Ziel meiner
+        Arbeit ist es, Menschen zu helfen, sich zu entspannen und zu erholen.
+      </p>
+
+      <p className=" text-xl tracking-wider leading-relaxed">
+        Ich bin Meditations- und Yoga-Lehrerin und ich liebe es, Menschen zu
+        helfen, sich zu entspannen und zu erholen. Ich bin spezialisiert auf
+        Yoga und Meditation für Anfänger und Fortgeschrittene. Das Ziel meiner
+        Arbeit ist es, Menschen zu helfen, sich zu entspannen und zu erholen.
+      </p>
+      <div className="group w-fit">
+        <Button
+          className=" mb-10 mt-5 bg-[var(--bg-background-color-6)] group-hover:text-white duration-300 transition-colors"
+          variant="outline"
+        >
+          <CalendlyEmbed url="https://calendly.com/ebysrelaxing/astrologie?background_color=fff7f2&text_color=6a604d&primary_color=6a604d" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Leistungen;
