6ded0b2f8a3857e9bf829b9722789bd602d8cf04
hakan <hbilgic1992@gmail.com>
2024-04-07 10:23:00 +0200

    center logo in the footer for md

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
   return (
     <div className="bg-[var(--bg-background-color-1)] col-span-5 flex items-center self-end px-4">
       <div className="flex flex-col md:flex-row  md:justify-between  w-full items-center">
-        <div className="flex items-center gap-4 self-start mb-3 md:mb-0 ">
+        <div className="flex items-center gap-4 self-start md:self-center mb-3 md:mb-0 ">
           <Logo2 className="w-12 h-12 md:ml-4" />
           <h1 className="text-2xl text-slate-800">ebysrelaxing</h1>
         </div>
